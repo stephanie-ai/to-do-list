b0da56000f4c44656b03c7b8f908073ec8429324
stephanie-ai <stephanie_cooper@hotmail.com>
2020-11-28 17:53:43 +0000

    create about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <>
+        <h1>About</h1>
+        <p>Welcome to your To Do List App</p>
+        </>
+    )
+}
+
+export default About;
