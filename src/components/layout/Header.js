7359af7fb7da021bbf626405183e4f4ffc19ac4f
stephanie-ai <stephanie_cooper@hotmail.com>
2020-11-28 17:53:27 +0000

    add header with navbar links

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,13 @@
 // function based component
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './style.css';
 
 function Header() {
     return (
         <header>
             <h1>To Do List</h1>
+            <Link to="/">Home</Link> | <Link to="/about">About</Link>
         </header>
     )
 }
