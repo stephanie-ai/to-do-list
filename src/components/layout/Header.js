7bbcdea2fd155e8a0bb63652b0a21a612370b47a
stephanie-ai <stephanie_cooper@hotmail.com>
2020-11-28 11:23:55 +0000

    add a header

---
+++ src/components/layout/Header.js
@@ -0,0 +1,13 @@
+// function based component
+import React from 'react';
+import './style.css';
+
+function Header() {
+    return (
+        <header>
+            <h1>To Do List</h1>
+        </header>
+    )
+}
+
+export default Header;
